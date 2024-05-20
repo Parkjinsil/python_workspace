fdc55d7ad256f74633277d8785e743268d6b9996
Parkjinsil <133620283+Parkjinsil@users.noreply.github.com>
2024-05-20 17:43:34 +0900

    0520feat

--- 16_numpy.py
+++ 16_numpy.py
@@ -110,3 +110,225 @@ print(a)
 k = np.random.random((2,2))
 print(k)
 
+c = np.arange(1,5)
+print(c)  # [ 1 2 3 4 ]
+c2 = np.array([1,2,3,4])
+print(c2) # [ 1 2 3 4 ]
+print(c.ndim) # 1   -> ndim : 차원을 보여줌
+print(c.shape) # (4, )    -> 1차원 배열 , 그 길이가 4
+# ex) (3, 4) -> 2차원 배열, 첫번째 차원의 크기가 3, 두번째 차원의 크기가 4
+# ex) (2, 2, 2) -> 3차원 배열, 첫번째 차원의 크기 2, 두번째 차원의 크기 2, 세번째 차원의 크기 2
+
+c.shape = (2,2) # 4x1 => 2x2 
+print(c)
+print(c.ndim) # 2
+c.shape = (1,4)
+print(c) # [[1 2 3 4]]
+
+
+b = np.full((2,2),3)
+print(b)
+
+import random
+k = np.random.random((2,2))
+print(k)
+x = random.random()
+print(x) # 스칼라
+
+na = np.array([1,2,3,4,5,6,7,8])
+a = na.reshape(2,4)
+print(a)
+# [[1 2 3 4]
+# [5 6 7 8]]
+
+print(a[1,1]) # 1행 1열 요소 -> 값 6
+print(a[1][1]) # 1행 1열 요소 -> 값 6
+
+array = np.array([
+  [[0,1,2,3],
+   [4,5,6,7],
+   [8,9,10,11]],
+  [[0,1,2,3],
+   [4,5,100,7],
+   [8,9,10,11]]
+])
+print(array, array.shape) # array.shsape : (2,3,4)
+
+array = np.array([
+  0,1,2,3,4,5,6,7,8,9,10,11,
+  0,1,2,3,4,5,100,7,8,9,10,11
+])
+array1 = array.reshape(2,3,4)
+print(array1)
+print(array1[1,1,2], array1[1][1][2]) # 100 100
+
+# 4칙 연산
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+# float64 64bit = 64/8 = 8 byte 실수형
+# int32 32bit = 32/8 = 4 byte 정수형
+
+print(x+y) # [[ 6.  8.], [10. 12.]]
+print(np.add(x,y))
+
+print(x-y)
+print(np.subtract(x,y))
+
+print(x*y)
+print(np.multiply(x,y))
+
+print(x/y)
+print(np.divide(x,y))
+
+v = np.array([9,10])
+w = np.array([11,12])
+
+# 벡터의 내적
+print(v.dot(w))
+print(np.dot(v,w)) # 219
+
+# 행렬과 벡터의 곱
+print(x.dot(v))
+print(np.dot(x,v)) # [29. 67.]
+
+# 행렬곱
+print(x.dot(y))
+print(np.dot(x,y))
+
+# 역행렬 구하는 법
+A = np.array([[1,2],
+             [3,4]])
+A_inv = np.linalg.inv(A)
+print(A_inv)
+
+a = [1,2,3]
+b = [3,4,5]
+print(a)
+print(b)
+print(type(a)) # list
+# list 연산 -> 사칙연산 안됨
+print(2*a) # [1, 2, 3, 1, 2, 3]
+print(a+b) # [1, 2, 3, 3, 4, 5]
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(a)
+print(b)
+print(type(a)) # <class 'numpy.ndarray'>
+# np.array 사칙연산 가능
+print(2*a) # [2 4 6]
+print(a+b) # [5 7 9]
+
+c = np.array([[1,2,3],
+             [4,5,6]]) # 2차원 배열 생성
+print(c.size) # 6
+print(c.shape) # (2,3)
+
+d = np.arange(10).reshape(2,5)
+print(d)
+print(np.arange(2,3,0.1))
+print(np.linspace(1.0,4.0,6))
+
+print(np.zeros((2,2),int))
+print(np.zeros((2,2),float))
+print(np.ones((2,2))) # 2x2 float 1 행렬
+print(np.full((2,2),7)) # 2x2 행렬을 7로 채움
+print(np.eye(2)) # identity matrix : 단위행렬
+print(np.random.random((2,2)))
+
+a = np.array([[1,2,3,4],
+              [5,6,7,8],
+              [9,10,11,12]])
+print(a)
+print(a[0,0]) # 1
+print(a[1,2]) # 7
+a[-1,-1] += 10
+print(a)
+print(a[1,:]) # [5 6 7 8]
+print(a[:2,:]) # 0부터 2미만행, 모든 열
+print(a[:2,1:3])
+print(a[:,0:3:2]) # 모든행, 0부터 3미만 2간격 모든열
+print(a[:,-1:-4:-2]) # [[4 2] [8 6] [22 10]]
+print(a[:,[3,0,2,1]]) # 모든행, 3,0,2,1열 순서로
+print(a[[0,1,2],[0,1,2]]) # [0,0][1,1][2,2]원소 -> [1 6 11]
+
+print(a>5) # a의 원소 중 5보다 큰 것 True로 표시, 아니면 False
+print(a[a>5]) # [ 6  7  8  9 10 11 22] : a의 원소 중 5보다 큰 것
+b = a[:] # 원본에 영향을 미침 / list는 이렇게 하면 값 복사였음(원본에 영향 X)
+# b = a.copy() 값 복사 (원본에 영향 X)
+print(b)
+b[b%2==0] +=100 # b의 원소 중 짝수인 원소에 100을 더함
+print(b)
+print(a)
+
+c = np.array([[1,2,3],
+              [4,5,6]])
+print(c)
+d = np.arange(11,17).reshape(2,3)
+print(d)
+
+print(c+d) # 원소별 연산
+print(c-d) # 원소별 연산
+print(c*d) # 원소별 연산
+print(c/d) # 원소별 연산
+print(c**2)
+print(np.sqrt(c)) # 제곱근
+print(np.dot(c, d.transpose())) # 행렬 연산
+# transpose() : 전치 행렬 -> 주어진 행렬의 행과 열을 서로 바꾼 새로운 행렬
+# ex) [[1 2 3] [4 5 6]] -> [[1 4][2 5][3 6]]
+
+print(np.dot(c.T, d)) # c.T : c의 전치행렬
+
+'''
+array.var()
+: 배열의 분산 계산
+  분산은 데이터가 평균값으로부터 퍼져있는 정도를 나타내는 척도
+  
+array.std()
+: 배열의 표준편차
+  표준편차는 데이터가 평균값으로부터 퍼져있는 정도를 나타내는 또 다른 척도
+'''
+
+# 숫자형 : int32 (정수형 32 비트 -> 정수형의 기본형)
+# 실수형 : float64 (실수형 64비트 -> 실수형의 기본형)
+ar = np.array([1,2,3.0])
+print('자료형 :',ar.dtype)
+
+# 실습 1
+# 숫자 15에서 23까지 배열을 만들고 이를 3*3 행렬로 변환하여 출력하라.
+array = np.arange(15,24).reshape(3,3)
+print(array)
+
+# 실습 2
+array = np.arange(1,5)
+array1 = np.arange(5,9)
+print(array)
+print(array1)
+# 위의 두 array를 이용해서 아래와 같은 값이 나오도록 코딩하라
+# [False False False False]
+print(array>array1)
+
+# 실습 3
+# 아래와 같이 3학생이 중간고사 점수, 기말고사 점수, 과제점수가 있다.
+#           (철수, 영희, 민재)
+# 중간고사 = (15, 100, 45)
+# 기말고사 = (78,  25, 90)
+#     과제 = (81,  45, 99)
+
+m = np.array([15, 100, 45]) 
+f = np.array([78, 25, 90])
+p = np.array([81, 45, 99])
+
+# 각 중간, 기말, 과제점수의 가중치를 각각 0.4, 0.4, 0.2를 부여하여 가중치의 합을 구하고, 
+# 거기에 기타점수 5점을 더하고자 한다.
+# 이를 array를 이용해 계산하는 프로그램을 짜 보아라.
+
+score = 0.4* m + 0.4* f + 0.2* p
+print(score)
+print('철수의 최종점수 :',score[0])
+print('영희의 최종점수 :',score[1])
+print('민재의 최종점수 :',score[2])
+
+print('중간고사 평균 :',np.mean(m))
+print('중간고사 표준편차 :',np.std(m))
+
---
+++ 17_matplotlib.py
@@ -0,0 +1,178 @@
+import matplotlib.pylab as plt
+import numpy as np
+
+# plt.plot([1,2,3,4]) # = plt.plot([0,1,2,3],[1,2,3,4])
+# plt.xlabel('x label')
+# plt.ylabel('y label')
+# plt.title('my xy graph')
+# plt.show()
+
+# x = np.arange(10)
+# plt.plot(x**2)
+# plt.show()
+
+# x = np.arange(10)
+# plt.plot(x**2)
+# plt.axis([0,100,0,100]) # 범위를 지정함
+# plt.show()
+
+# x = np.arange(-20,20) # -20에서 20 사이의 수를 1의 간격으로 생성
+# y1 = 2*x 
+# y2 = (1/3)*x**2+5
+# y3 = -x**2-5
+# # 녹색 점선 원기호와,빨간색 실선 세모기호, 파랑색 별표와 점선으로 함수를 표현
+# plt.plot(x,y1,'go-',x,y2,'r^-',x,y3,'b*:')
+# plt.axis([-30,30,-30,30]) # 그림 그릴 영역 지정함
+# plt.show()
+
+# N = 50
+# x = np.arange(N)
+# y = np.random.random(size=N)
+
+# plt.plot(x,y,'g^:') # 녹색 점선 세모표시
+# plt.show()
+
+from IPython.display import Image
+
+# x = np.linspace(0,np.pi*2, 100) # start에서 stop 까지 균등한 간격으로 step개 숫자 생성
+# fig = plt.figure()
+# plt.plot(x,np.sin(x), 'r-') # 사인함수는 빨간 실선
+# plt.plot(x,np.cos(x),'b:')  # 코사인함수는 파란 점선
+# fig.savefig('sin_cos_fig.png')
+# # Image('sin_cos_fig.png') # Jupyter Notebook이나 JupyterLab과 같은 노틉툭 환경에서만 이미지 출력 실행됨
+# plt.show() # 일반 Python 스크립트나 REPL 환경에서는 Image 함수가 작동하지 않으므로 plt.show() 해야지만 출력됨
+
+# plt.style.use('dark_background')
+x = np.linspace(0,np.pi*2, 100)
+
+# plt.title('Sin cos curve')
+# plt.rc('font',family='Malgun Gothic') # 한글로 제목 가능
+# plt.title('사인 코사인 커브') 
+# plt.plot(x, np.sin(x),'r-',label = 'sin curve', c = 'yellow') # 컬러 지정도 가능
+# plt.plot(x, np.cos(x), 'b:', label = 'cos curve')
+# plt.xlabel('x value') # x축에 이름 붙이기
+# plt.ylabel('y value')
+# # plt.legend() # -> 범례 표시 label을 plot에 넣어줘야 표시됨
+# plt.legend(loc='upper right') # 위쪽 오른편에 범례 표시
+# plt.show()
+
+# t = np.arange(0,10,2) 
+# plt.plot(t,t) # plt를 여러번 호출해서 함수를 여러 개 그릴 수 있음
+# plt.plot(t,t**2)
+# plt.plot(t,t**3)
+
+# # plt.plot(t,t, t,t**2, t, t**3) # 이렇게 한번에 쓸 수도 있음
+
+# plt.xlabel('x datas')
+# plt.ylabel('y datas')
+# plt.grid() # 이렇게 눈금 칸을 이용해서 더 확실하게 살펴볼 수 있음
+# plt.show()
+
+# plt.scatter(3,5) # (3,5)좌표에 점 찍기
+# plt.title('practice scatter function', fontsize=15)
+# plt.show()
+
+# x = np.arange(0,10,1)
+# plt.scatter(x, x**2, c = 'k', s = 10) # s를 이용해서 점의 크기를 변경할 수 있음
+# plt.text(3,50,'y=x^2 graph') # 임의로 원하는 좌표에 텍스트 추가하기
+# plt.show()
+
+'''
+* 선의 종류
+디폴트가 직선, 점으로 표현하는 마커나 점선 선택 가능
+선의 선택은 plot에서 세번째 인자 -> 선의 종류
+                    네번째 인자 -> 색 지정
+                    
+* 타이틀 : plt.title('타이틀명')
+* X축 라벨 : plt.xlabel('X축 라벨명')
+* Y축 라벨 : plt.ylabel('y축 라벨명')
+
+* 구간 확대/축소
+: 그래프는 입력되는 x,y의 최소,최대 구간으로 자동으로 그려짐
+  이 구간 키우거나 줄이려면 -> x축 : plt.xlim(최소, 최대)
+                             y축 : plt.ylim(최소, 최대)
+                             
+* 레전드
+: 그래프를 그릴 때 여러개의 그래프를 같익 그릴 수 있는데, 이 경우 각 그래프가 구분이 안됨
+ -> 그래프마다 라벨을 달 수 있음 plt.legend('위치')
+ 
+* 어노테이션
+: 그래프에 화살표를 그린 후, 그 화살표에 문자열을 출력하는 기능
+  ex) plt.annotate('문자열',xy,xytext,arrowprops)
+     : xy -> 화살표가 가르키는 점의 위치
+       xytext -> 문자열이 출력될 위치
+       arrowprops -> 화살표의 속성으로 칼라등을 정의
+'''
+# x = np.arange(1,10)
+# y = x*5
+
+# plt.plot(x,y)
+# plt.annotate('annotate',xy=(2,10),xytext=(5,20),arrowprops={'color':'green'})
+# plt.show()
+
+# 스타일 지정
+# plt.plot([10,20,30,40],[1,4,9,16],'rs--')
+# plt.show()
+
+# plt.plot([10,20,30,40],[1,4,9,16],c='b', lw=5,ls='--',marker='o',ms=15,mec='g',mew=5,mfc='r')
+# plt.show()
+
+# titleFont = {'fontsize': 20, 'color':'#0000ff'}
+
+# plt.subplot(211).set(xticks=[-4,-2,0,2,4], yticks=[-1,0,1])
+# # 211 -> 2행 1열의 첫번째 그래프
+# triXAxis = np.linspace(-4,4,100)
+# sinYAxis = np.sin(triXAxis)
+# cosYAxis = np.cos(triXAxis)
+# plt.plot(triXAxis, sinYAxis, label = "Sin Funtion", linestyle='--')
+# plt.plot(triXAxis, cosYAxis, label = "Cos Function", linestyle='-.')
+# plt.legend()
+# # plt.title('Trigonometric Functions', fontdict=titleFont)
+
+# # 축의 간격 설정 : plt.xticks(), plt.yticks()
+# #                -> list 형으로 넣고싶은 눈금의 숫자를 입력해줘야 함
+# # plt.xticks([-4,-2,0,2,4])  -> plt.subplot().set()함수로 간단하게 쓸 수 있음
+# # plt.yticks([1,0,-1])
+# plt.title('Trigonometric Functions', loc = 'right') # loc 매개변수는 제목을 정렬해주는 변수
+
+# plt.subplot(212).set(xlim=[0,8],ylim=[0,3])
+# # 212 -> 2행 1열의 두번째 그래프
+# logXAxis = np.linspace(1,10,100)
+# logYAxis = np.log(logXAxis)
+# plt.plot(logXAxis, logYAxis)
+# plt.title('log Function')
+# plt.show()
+
+'''
+* 다양한 종류의 그래프를 한 화면에 여러개 그리기에 편리 -> subplots() 함수
+ : subplots() 함수는 figure 객체(컨테이너 객체)와 Axes 객체(그림 그릴 수 있는 테두리 상자 모양)를 반환함
+
+fig, ax = matplotlib.pyplot.subplots( nrows = 2, ncols = 2, ...)
+-> fig : 그리기 요소들을 포함하는 컨테이너 (여러 개의 Axes를 가질 수 있음)
+   ax  : 그림을 그릴 수 있는 공간 Axes (Axes 객체)    !=    Axis 객체 : x축, y축
+   mayplotlib.pyplot : 필요한 모듈
+   nrows : 행의 수
+   ncols : 열의 수
+'''
+
+# fig, ax = plt.subplots(2,2) # 2행 2열의 그림을 그리는 공간을 만든다
+
+# X = np.random.randn(100) # 정규 분포를 가지는 데이터
+# Y = np.random.randn(100) # 정규 분포를 가지는 데이터
+# ax[0,0].scatter(X,Y)     # 산점도 그림
+# X = np.arange(10)
+# Y = np.random.uniform(1,10,10) # 균일 분포값 생성
+# ax[0,1].bar(X,Y)         # 막대 차트 그림
+# X = np.linspace(0,10,100)
+# Y = np.cos(X)
+# ax[1,0].plot(X,Y)        # 실선으로 함수를 그림
+# Z = np.random.uniform(0,1,(5,5))
+# ax[1,1].imshow(Z)        # 분포를 2D 이미지로 그림
+# plt.show()
+
+# subplots로 12개 그리기
+fig, axes = plt.subplots(3,4)
+axes[1,2].plot([1,2,3,4]) # subplots으로 반환한 axes는 배열처럼 접근 가능
+axes[2,1].plot([1,2,3,4],[1,2,3,4])
+plt.tight_layout() # subplot간 margin이 겹치지 않게 하는 함수
+plt.show()
